142d28c14fe12d63f4a2a10b8cd0857dbef3696e
anyelams <u20211195450@usco.edu.co>
2025-09-28 16:51:09 -0500

    feat(auth): configurar layout con Stack para pantallas de autenticación

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,51 +1,13 @@
-import "react-native-gesture-handler";
-import { Ionicons } from "@expo/vector-icons";
-import { Tabs } from "expo-router";
-import { Platform } from "react-native";
+// app/(auth)/_layout.jsx
+import { Stack } from "expo-router";
 
-export default function TabLayout() {
+export default function AuthLayout() {
   return (
-    <Tabs
+    <Stack
       screenOptions={{
         headerShown: false,
-        tabBarActiveTintColor: "#000",
-        tabBarInactiveTintColor: "#000",
-        tabBarLabelStyle: { color: "#000" },
-        tabBarStyle: {
-          height: 55,
-          paddingTop: 2,
-          paddingBottom: 2,
-          ...Platform.select({ ios: { position: "absolute" }, default: {} }),
-        },
+        contentStyle: { marginHorizontal: 0, paddingHorizontal: 0 },
       }}
-    >
-      <Tabs.Screen
-        name="notifications"
-        options={{
-          title: "Notificaciones",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="notifications-outline" size={22} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="home"
-        options={{
-          title: "Inicio",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="home-outline" size={22} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Perfil",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="person-outline" size={22} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    />
   );
 }
