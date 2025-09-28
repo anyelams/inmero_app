06ec9408bf8616295e7db57d159a1089e20ea939
anyelams <u20211195450@usco.edu.co>
2025-09-28 17:06:18 -0500

    feat(tabs): configurar layouts de autenticación y tabs

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,13 +1,78 @@
-// app/(auth)/_layout.jsx
-import { Stack } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { Platform } from "react-native";
+import "react-native-gesture-handler";
+import { colors } from "../../config/theme";
+import { typography } from "../../config/typography";
 
-export default function AuthLayout() {
+export default function TabLayout() {
   return (
-    <Stack
+    <Tabs
       screenOptions={{
         headerShown: false,
-        contentStyle: { marginHorizontal: 0, paddingHorizontal: 0 },
+        tabBarActiveTintColor: colors.secondary,
+        tabBarInactiveTintColor: colors.textSec,
+        tabBarLabelStyle: {
+          ...typography.medium.small,
+        },
+        tabBarStyle: {
+          height: 70,
+          paddingTop: 8,
+          paddingBottom: 12,
+          backgroundColor: colors.white,
+          borderTopWidth: 0.5,
+          borderTopColor: colors.border,
+          elevation: 8,
+          shadowColor: colors.text,
+          shadowOffset: { width: 0, height: -2 },
+          shadowOpacity: 0.1,
+          shadowRadius: 8,
+          ...Platform.select({
+            ios: { position: "absolute" },
+            default: {},
+          }),
+        },
       }}
-    />
+    >
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          title: "Notificaciones",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "notifications" : "notifications-outline"}
+              size={22}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Inicio",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              size={22}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Perfil",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "person" : "person-outline"}
+              size={22}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
