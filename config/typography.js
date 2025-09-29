a1e12aba1f7e8a52f2a691bcbf8800b15f1addca
anyelams <u20211195450@usco.edu.co>
2025-09-28 22:58:50 -0500

    chore(typography): ajustar tamaños de fuente según WCAG

--- config/typography.js
+++ config/typography.js
@@ -3,8 +3,6 @@ export const typography = {
     big: { fontFamily: "RobotoBold", fontSize: 22 }, // Títulos principales
     large: { fontFamily: "RobotoBold", fontSize: 20 }, // Títulos de pantalla
     medium: { fontFamily: "RobotoBold", fontSize: 18 }, // Subtítulos importantes
-    regular: { fontFamily: "RobotoBold", fontSize: 16 }, // Botones y labels
-    small: { fontFamily: "RobotoBold", fontSize: 14 }, // Textos pequeños bold
   },
   semibold: {
     big: { fontFamily: "RobotoSemiBold", fontSize: 20 }, // Títulos secundarios
@@ -17,14 +15,14 @@ export const typography = {
     big: { fontFamily: "RobotoMedium", fontSize: 18 }, // Títulos de cards
     large: { fontFamily: "RobotoMedium", fontSize: 16 }, // Subtítulos de cards
     medium: { fontFamily: "RobotoMedium", fontSize: 14 }, // Texto medio
-    regular: { fontFamily: "RobotoMedium", fontSize: 12 }, // Labels normales
-    small: { fontFamily: "RobotoMedium", fontSize: 10 }, // Texto pequeño medium
+    regular: { fontFamily: "RobotoMedium", fontSize: 13 }, // Labels normales
+    small: { fontFamily: "RobotoMedium", fontSize: 12 }, // Texto pequeño medium
   },
   regular: {
     big: { fontFamily: "RobotoRegular", fontSize: 16 }, // Tu original
     large: { fontFamily: "RobotoRegular", fontSize: 15 }, // Tu original
     medium: { fontFamily: "RobotoRegular", fontSize: 14 }, // Tu original
-    regular: { fontFamily: "RobotoRegular", fontSize: 12 }, // Tu original
-    small: { fontFamily: "RobotoRegular", fontSize: 10 }, // Tu original
+    regular: { fontFamily: "RobotoRegular", fontSize: 13 }, // Tu original
+    small: { fontFamily: "RobotoRegular", fontSize: 12 }, // Tu original
   },
 };
